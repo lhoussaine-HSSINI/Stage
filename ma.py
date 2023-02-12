59d357e1fd3c6933aaa292562c2d069019866e5a
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-02-12 15:43:17 +0000

    aa

--- ma.py
+++ ma.py
@@ -120,5 +120,6 @@ st.markdown("""
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 """, unsafe_allow_html=True)
-time.sleep(15)
 html(my_html)
+from streamlit_javascript import st_javascript
+st_javascript(my_js)
