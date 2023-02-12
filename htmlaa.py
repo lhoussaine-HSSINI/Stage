7b2023cac29e922f41406bff3d6670ab6b26fa94
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-02-12 16:01:07 +0000

    aa

---
+++ htmlaa.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from streamlit.components.v1 import html
+
+# Define your javascript
+my_js = """
+alert("Hola mundo");
+console.log("hello lhoussain");
+"""
+
+# Wrapt the javascript as html code
+my_html = f"<script>{my_js}</script>"
+
+# Execute your app
+st.title("Javascript example")
+html(my_html)
