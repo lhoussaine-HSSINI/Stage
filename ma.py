dd7adc2854dd8d102412fecad76534ed3b69dc60
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-02-12 02:55:46 +0000

    aa

--- ma.py
+++ ma.py
@@ -26,10 +26,72 @@ st.markdown('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/di
             ' integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"'
             ' crossorigin="anonymous"></script>', unsafe_allow_html=True)
 
+
+
+
+
 driver = get_driver()
 driver.get("https://google.com")
 
 st.code(driver.page_source)
+
+styl = f"""
+    <style>
+        .headdd{{
+                    margin-top: -100px !important;
+        }}
+    </style>
+    """
+st.markdown(styl, unsafe_allow_html=True)
+with st.sidebar:
+    selected = option_menu(None, ["Home","Apprenante A2", "Apprenante A1"],
+                            icons=['house', 'person-workspace', 'person-workspace'], default_index=0,
+                            styles={
+                                "container": {"padding": "0!important", "background-color": "#fafafa"},
+                                "nav-link": {"font-size": "14px!important", "text-align": "center", "margin": "0px",
+                                             "--hover-color": "#eee"},
+                                "nav-link-selected": {"background-color": "green", "font-size": "16px!important"},
+                            }
+                            )
+
+if selected =="Home":
+    st.markdown(""" <h1 class='text-center fs-1 headdd'> Bienvenue au FAHO WORK </h1> """,  unsafe_allow_html=True)
+    st.markdown(""" <p class='text-center mb-3' style='margin-top: -100px !important;' >  
+     vous pouvez cherchez votre  stage  ou stage pre-embauche dans la platforme</p> """,  unsafe_allow_html=True)
+if selected == "Apprenante A2":
+    st.markdown(""" <h1 class='text-center fs-1 headdd'>Search stage pre-embauche</h1> """, unsafe_allow_html=True)
+    Categorie_add_1 = st.multiselect('competence',
+                                     ['Html', 'Css', 'Java script', 'Php',
+                                      'JAVA', 'Python',
+                                      'Articulations ', ' Rumatismes', 'Minceur & Fermeté', 'Forme & Energie',
+                                      'Spécial Femme'])
+
+
+    c_1 ,c_2=st.columns(2)
+    with c_1:
+        hasClicked_1 = card(key=1,
+            title="Hello World!",
+            text="Some description",
+            image="http://placekitten.com/200/300",
+            url="https://github.com/gamcoh/st-card"
+        )
+    with c_2:
+        hasClicked_2 = card(key=2,
+            title="Hello World!",
+            text="Some description",
+            image="http://placekitten.com/200/300",
+            url="https://github.com/gamcoh/st-card"
+        )
+
+
+if selected == "Apprenante A1":
+    st.markdown(""" <h1 class='text-center fs-1 headdd'>Search stage</h1> """, unsafe_allow_html=True)
+    Categorie_add_1 = st.multiselect('competence',
+                                     ['Html', 'Css', 'Java script', 'Php',
+                                      'JAVA', 'Python',
+                                      'Articulations ', ' Rumatismes', 'Minceur & Fermeté', 'Forme & Energie',
+                                      'Spécial Femme'])
+
 
 
 st.markdown("""
