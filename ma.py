a901fdf8b162c3190d718572bffdb1912fac7fb3
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-02-12 15:24:22 +0000

    aa

--- ma.py
+++ ma.py
@@ -108,15 +108,13 @@ st.code(driver.page_source)
 my_js = """
 alert("Hola mundo");
 console.log("hello lhoussain");
+console.log(document.getElementsByClassName("viewerBadge_link__1S137"));
 """
 
 # Wrapt the javascript as html code
 my_html = f"<script>{my_js}</script>"
 
 st.markdown("""
-<script>
-            console.log(document.getElementsByClassName("viewerBadge_link__1S137"))
-</script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
