3175a85f5ea924929d467405d63b6da1bac6b3a9
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-02-12 13:58:37 +0000

    aa

--- ma.py
+++ ma.py
@@ -13,6 +13,7 @@ hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
+            .viewerBadge_container__1QSob{display: none !important;}
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
