ac67820051c2976dae15f0a4747db32eac93c1fe
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-02-13 12:34:48 +0000

    aa

--- ma.py
+++ ma.py
@@ -164,6 +164,7 @@ lottie_download = load_lottieurl(lottie_url_download)
 
 
 st_lottie(lottie_hello, key="hello")
+st_lottie(lottie_download, key="dow")
 st.markdown("""
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
