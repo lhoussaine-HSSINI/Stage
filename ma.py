3538c22faec7d9f723af84616444e42fc4e1f73b
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-02-15 16:13:08 +0000

    aa

--- ma.py
+++ ma.py
@@ -188,6 +188,18 @@ if selected == "Apprenante A2":
     list_company_name=[]
     list_=[]
 
+    i_counter = 2
+
+    while True:
+        if i_counter <= page_total_of_search:
+            driver.get(f"https://ma.indeed.com/jobs?q=stage+web&fromage=1&start={i_counter - 1}0")
+            list_li_1 = driver.find_elements(by=By.CSS_SELECTOR,
+                                             value="div[class='slider_container css-g7s71f eu4oa1w0']")
+            st.code(len(list_li_1))
+            st.code(f"https://ma.indeed.com/jobs?q=stage+web&fromage=1&start={i_counter - 1}0")
+            i_counter += 1
+        break
+
     for i in range(len(list_li)):
         # st.markdown(list_li[i].find_element(by=By.CSS_SELECTOR, value="div[class='css-1m4cuuf e37uo190']").text)
         try:
@@ -221,17 +233,7 @@ if selected == "Apprenante A2":
                             </a>
                             """, unsafe_allow_html=True)
 
-    i_counter = 2
 
-    while True:
-        if i_counter <= page_total_of_search:
-            driver.get(f"https://ma.indeed.com/jobs?q=stage+web&fromage=1&start={i_counter - 1}0")
-            list_li_1 = driver.find_elements(by=By.CSS_SELECTOR,
-                                             value="div[class='slider_container css-g7s71f eu4oa1w0']")
-            st.code(len(list_li_1))
-            st.code(f"https://ma.indeed.com/jobs?q=stage+web&fromage=1&start={i_counter - 1}0")
-            i_counter += 1
-        break
 if selected == "Apprenante A1":
     st.markdown(""" <h1 class='text-center fs-1 headdd'>Search stage</h1> """, unsafe_allow_html=True)
     Categorie_add_1 = st.multiselect('competence',
