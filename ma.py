b9408ac7eca939ba747d7b571ec34d9e8e65d9f3
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-02-15 14:07:52 +0000

    aa

--- ma.py
+++ ma.py
@@ -190,9 +190,9 @@ if selected == "Apprenante A2":
 
     while True:
         if i_counter <= page_total_of_search:
-            # driver.get(f"https://ma.indeed.com/jobs?q=stage+web&fromage=1 & start = {i_counter - 1}0")
-            # list_li_1 = driver.find_elements(by=By.CSS_SELECTOR,value="div[class='slider_container css-g7s71f eu4oa1w0']")
-
+            driver.get(f"https://ma.indeed.com/jobs?q=stage+web&fromage=1&start={i_counter-1}0")
+            list_li_1 = driver.find_elements(by=By.CSS_SELECTOR,value="div[class='slider_container css-g7s71f eu4oa1w0']")
+            st.code(len(list_li_1))
             st.code(f"https://ma.indeed.com/jobs?q=stage+web&fromage=1&start={i_counter-1}0")
             i_counter += 1
         break
