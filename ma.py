e1ad3072bbf54b4e0d281bce0869aad805b50590
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-02-16 09:36:32 +0000

    aa

--- ma.py
+++ ma.py
@@ -270,6 +270,7 @@ if selected == "Apprenante A2":
     st.markdown(counttt)
     for  ii in range(len(list_link_job)):
         driver.get(list_link_job[ii])
+        st.code(list_link_job[ii])
         list_linkk = driver.find_element(by=By.ID,value="div[id='jobDescriptionText']")
         st.markdown(list_linkk)
 
