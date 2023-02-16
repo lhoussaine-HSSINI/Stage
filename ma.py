d5903ad0c51be6e919cb077a6a699d56abfc85bc
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-02-16 13:32:47 +0000

    aa

--- ma.py
+++ ma.py
@@ -219,6 +219,7 @@ if selected == "Apprenante A2":
         except:
             list_discription.append("none")
     display_data()
+    driver.quit()
 if selected == "Apprenante A1":
     st.markdown(""" <h1 class='text-center fs-1 headdd'>Search stage</h1> """, unsafe_allow_html=True)
     Categorie_add_1 = st.multiselect('competence',
@@ -259,6 +260,7 @@ if selected == "Apprenante A1":
         except:
             list_discription.append("none")
     display_data()
+    driver.quit()
 st.markdown("""
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
