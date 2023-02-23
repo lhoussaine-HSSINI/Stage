ee80d260a46fdec92850f4c164c919d0c6954a9b
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-02-23 00:09:59 +0000

    aa

--- query.py
+++ query.py
@@ -1,11 +1,13 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 import  streamlit as st
 import requests
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def gettt():
     options = Options()
